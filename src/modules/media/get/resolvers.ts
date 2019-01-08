d6c0264b0cc3e434ca5897e0435ab2603cc50ec2
Viktor Bengtsson <vizibit@gmail.com>
2019-01-07 19:17:07 -0500

    Updated Media GraphQL Modules amd packages in package.json

--- src/modules/media/get/resolvers.ts
+++ src/modules/media/get/resolvers.ts
@@ -3,15 +3,13 @@ import * as bluebird from 'bluebird';
 import * as path from "path";
 
 interface IDirectory{
-    filePath: string;
-    isFileBoolean: boolean;
+    filePath: string|undefined;
     files: [string];
 }
 
 interface IFile{
-    filePath: string;
+    filePath: string|undefined;
     name: string;
-    isFileBoolean: boolean;
 }
 
 const fsPromise: any = bluebird.promisifyAll(require('fs'));
@@ -21,36 +19,27 @@ export const resolvers: ResolverMap = {
         getFiles: async () => {
             const pathToMedia = path.join(__dirname, "../../../../public/images");
 
-            const func = async (dir: any) => {
+            const call = async (dir: any) => {
                 const arrayOfFileNameStrings = await fsPromise.readdirAsync(dir);
                 const fileArray = arrayOfFileNameStrings.map(async (fileName: string) => {
                     const fullPath = `${dir}/${fileName}`;
-                    const directoryData = await fsPromise.statAsync(fullPath);
                     const directory = {} as IDirectory
     
-                    if (directoryData.isDirectory()) {
-                        directory.filePath = fullPath;
-                        directory.isFileBoolean = directoryData.isFile();
-                        directory.files = await fsPromise.readdirAsync(fullPath).map(async (fileName: string) => {
-                            const filePath = `${directory.filePath}/${fileName}`;
-                            const fileData = await fsPromise.statAsync(filePath);
-                            const file = {} as IFile
-                            file.filePath = filePath;
-                            file.name = fileName;
-                            file.isFileBoolean = fileData.isFile()
-                            return file;
-                        });
-                    }
+                    directory.filePath = `${'static'}/${fullPath.split("/public/").pop()}`;
+                    directory.files = await fsPromise.readdirAsync(fullPath).map(async (fileName: string) => {
+                        const filePath = `${fullPath}/${fileName}`;
+                        const file = {} as IFile
+                        file.filePath = `${'static'}/${filePath.split("/public/").pop()}`;
+                        file.name = fileName;
+                        return file;
+                    });
+
                     return directory;
                 });
                 return Promise.all(fileArray);
             }
             
-            try {
-                console.log(await func(pathToMedia));
-            } catch (err) {
-                console.log(err);
-            }
+            return call(pathToMedia);
         }
     }
 }
