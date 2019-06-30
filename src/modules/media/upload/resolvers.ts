2d2fadc039868730b2073d1b0326aff0a218b0ce
Viktor Bengtsson <vizibit@gmail.com>
2019-06-30 18:11:10 -0400

    Updated path to images folder

--- src/modules/media/upload/resolvers.ts
+++ src/modules/media/upload/resolvers.ts
@@ -1,7 +1,8 @@
-import { createWriteStream, unlink } from 'fs';
+import { createWriteStream, unlink } from "fs";
 import { ResolverMap } from "../../../types/graphql-utils";
+import * as path from "path";
 
-const uploadDir = './public/images';
+const uploadDir = path.resolve(__dirname, "../../../../public/images");
 
 const processUpload = ({ stream, filename, directory }: any): Promise<any> => {
   const directoryTarget = `${uploadDir}/${directory}/${filename.toLowerCase()}`;
@@ -10,36 +11,37 @@ const processUpload = ({ stream, filename, directory }: any): Promise<any> => {
   return new Promise((resolve, reject) =>
     stream
       .pipe(createWriteStream(directoryTarget))
-      .on('finish', () => resolve({ path }))
-      .on('error', reject));
+      .on("finish", () => resolve({ path }))
+      .on("error", reject)
+  );
 };
 
 export const resolvers: ResolverMap = {
-    Query: {},
-    Mutation: {
-      async addMedia(_, { file, directory }) {
-          const { createReadStream, filename } = await file;
-          const stream = createReadStream()
+  Query: {},
+  Mutation: {
+    async addMedia(_, { file, directory }) {
+      const { createReadStream, filename } = await file;
+      const stream = createReadStream();
 
-          const { path } = await processUpload({ stream, filename, directory });
+      const { path } = await processUpload({ stream, filename, directory });
 
-          return { filename, path };
-      },
-      async removeMedia(_, { file, directory }) {
-        const directoryTarget = `${uploadDir}/${directory}/${file}`;
-
-        try {
-          await unlink(directoryTarget, (error) => {
-            if (error) {
-                console.log(error);
-                return;
-            }
-          })
-        } catch (error) {
-          return false;
-        }
-
-        return true;
-      }
+      return { filename, path };
     },
-}
+    async removeMedia(_, { file, directory }) {
+      const directoryTarget = `${uploadDir}/${directory}/${file}`;
+
+      try {
+        await unlink(directoryTarget, error => {
+          if (error) {
+            console.log(error);
+            return;
+          }
+        });
+      } catch (error) {
+        return false;
+      }
+
+      return true;
+    }
+  }
+};
