10edb4050d1eb6ed7978c1b47bbaf69644d4ce39
Viktor Bengtsson <vizibit@gmail.com>
2019-10-26 15:50:35 -0400

    Updated Loan GraphQL Modules with new find query

--- src/modules/loan/find/resolvers.ts
+++ src/modules/loan/find/resolvers.ts
@@ -1,5 +1,6 @@
 import { ResolverMap } from "../../../types/graphql-utils";
 import { Loan } from "../../../entity/Loan";
+import { getConnection } from "typeorm";
 
 export const resolvers: ResolverMap = {
   Query: {
@@ -7,6 +8,22 @@ export const resolvers: ResolverMap = {
       return Loan.findOne({
         where: { id }
       });
+    },
+
+    findPrivateLoan: async (_, { limit = 10, offset = 0 }, __) => {
+      let loanQB = await getConnection()
+        .getRepository(Loan)
+        .createQueryBuilder("loan");
+
+      loanQB.andWhere("loan.loan_type = 'privatlan'");
+
+      return loanQB
+        .take(limit)
+        .skip(offset)
+        .groupBy("loan.id")
+        .addOrderBy("amount_max", "DESC")
+        .addOrderBy("interest", "ASC")
+        .getMany();
     }
   }
 };
